2a5875e5941590a0b5891992943b9a64c3382834
LAPTOP-J6F0OR4J\TSelmeg <99040437@mydavinci.nl>
2017-01-25 12:05:12 +0100

    no message

--- lingo.js
+++ lingo.js
@@ -1,16 +1,12 @@
-function Lingo () {
+(function Lingo () {
 	var round = 0;
 	var count = 0;
-	function randomWord () {
-		
+	(function randomWord () {
 		var word = words[Math.floor(Math.random() * words.length)];
 		return word;
-	}
-	this.word = randomWord ();
+	})();
 	
-	function newRow (){
-		
-		
+	(function newRow (){	
 		for(var t=0; t< 5 ; t++){
 			var box = document.createElement("li");
 			box.id= "box"+ count;
@@ -20,51 +16,38 @@ function Lingo () {
 			document.getElementById("box" + count).style.display = "inline-block";
 			document.getElementById("box" + count).style.border = "2px solid blue";
 			count++
-
 				}
-		(function firstLetter () {
-			console.log(this.word);
 			count =count - 5;
 			var FirstLetter = this.word.charAt(0);
 			document.getElementById("box" + count).innerHTML = FirstLetter;
-			document.getElementById("box" + count).style.marginLeft = "200px";
-		})();		
-					var br = document.createElement("br");
+			document.getElementById("box" + count).style.marginLeft = "200px";	
+			var br = document.createElement("br");
 			document.body.appendChild(br);
-		
-	}
-	newRow();
+	})();
 	
-	function checkWord (input) {
+	function checkWord () {
 		var input = "";
 		for (var a = 0; a < 5; a++){
 			input = input + document.getElementById("box"+ count).innerHTML;
 			count++;
 		}
 		count = count - 5;
-loop1:
+		var used = 0;
 		for (var i = 0; i < input.length; i++) {
-			var red = true;
-			loop2:
-			for (var j = 0; j < word.length; j++) {
-				if(input[i] == word[j]){
-					red = false;
-					if(i == j){
-						document.getElementById("box"+ count).style.backgroundColor = "green";
-						break loop2;
-					}
-				}else{
-					console.log(red);
-					if(red == true && j == word.length - 1){
-						document.getElementById("box"+ count).style.backgroundColor = "red";
-					}else if(red == false && j == word.length - 1){
-						document.getElementById("box"+ count).style.backgroundColor = "yellow";
-						console.log("yellow")
-					}
-				}
+			if(word[i] == input[i]){
+				document.getElementById("box"+ count).style.backgroundColor = "green";
+			}else if(word[i] == input[0] ||word[i] == input[1] || word[i] == input[2] || word[i] == input[3] || word[i] ==input[4]){
+				document.getElementById("box"+ count).style.backgroundColor = "yellow";
+			}else{
+				document.getElementById("box"+ count).style.backgroundColor = "red";
 			}
-			count++
+			count++;
+			used++;
 		}
+		count = count - used;
+		console.log(count);
+		count = count + 5;
+		console.log(count);
 		round++;
 		if(round == 5){
 			alert("You lose!");
@@ -72,6 +55,4 @@ loop1:
 		newRow();
 	}
 	document.getElementById("check").onclick = checkWord;
-}
-
-Lingo();
+})();
